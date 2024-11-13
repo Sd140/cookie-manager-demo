d2b4d4fcd66771f7351b33b5ba7ba3b445e5aaff
ounkar.jagtap <ounkar.jagtap@idfy.com>
2024-11-13 12:08:44 +0530

    site improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import "./styles/styles.css"
+import "./styles/styles.css";
+import banner from "./assets/home_banner_bg.png";
+import scroll from "./assets/scroll_down.png";
+import idfyLogo from "./assets/idfy_logo.png"
 
 function App() {
   return (
     <div className="container">
       <header className="header">
-        <h1>Privy by IDfy</h1>
+        <div className="header-logo-section">
+          <h1>PRIVY</h1>
+          <p>by</p>
+          <img src={idfyLogo} alt="IDfy"></img>
+        </div>
         <nav className="nav">
           <a href="#about">About Us</a>
           <a href="#products">Products</a>
@@ -13,13 +20,116 @@ function App() {
         </nav>
       </header>
 
-      <main className="main-content">
+      <section className="home-banner">
+        <div className="home-banner-bg">
+          <img src={banner} alt="."></img>
+        </div>
+        <div className="home-banner-ellipse home-banner-ellipse-1"></div>
+        <div className="home-banner-ellipse home-banner-ellipse-2"></div>
+        <div className="home-banner-ellipse home-banner-ellipse-3"></div>
+        <div className="home-banner-main">
+          <div className="home-banner-text">
+            <div className="home-banner-slogan">
+              MANAGE COOKIES, GAIN COMPLIANCE
+            </div>
+            <div className="home-banner-title">
+              Customize Your Cookie Consent for Ultimate Compliance
+            </div>
+            <div className="home-banner-subtitle">
+              Leverage our unique solution to simplify cookie management
+            </div>
+          </div>
+        </div>
+        <div className="home-banner-footer">
+          <p><u>How would you like to manage your cookie settings today?</u></p>
+          <span>Scroll Down <img src={scroll} alt="" /></span>
+        </div>
+      </section>
+
+      <div className="section-31 data">
+        <div className="container-12">
+          <div className="home-data">
+            <div className="hd-header">
+              <h2 data-w-id="877904d2-8fa1-ad57-8a62-e5aaf389adda" style={{opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d"}} className="heading-76">
+                A comprehensive consent governance suite for all enterprises
+              </h2>
+              <div className="div-block-21 mt20">
+              </div>
+            </div>
+          </div>
+          <div className="hd-content">
+            <div data-w-id="a2ba9228-f7c1-f0c8-bf61-eb098024b59b" style={{opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d"}} className="hd-wrapper">
+              <div className="hd-inner">
+                <h2 className="heading-75">
+                  Inspect AI
+                </h2>
+                <div className="div-block-21">
+                </div>
+                <div className="paragraph-14px-3">
+                  The compliance co-pilot
+                </div>
+              </div>
+              <div className="paragraph-16px-4">
+                A tool designed for Privacy &amp; Info-sec teams to perform DPDP gap assessments in less <br/>than a minute.</div>
+              <div className="div-block-20">
+                <a href="/inspect-ai" className="link-6">
+                  Explore Inspect AI
+                </a>
+                <img src="https://cdn.prod.website-files.com/65f7dd1d0e60ce666af29650/668e6ca84e4730a2ef3fa896_arrow_forward.png" loading="lazy" alt=""/>
+              </div>
+            </div>
+            <div data-w-id="a2ba9228-f7c1-f0c8-bf61-eb098024b5a5" style={{opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d"}} className="hd-wrapper">
+              <div className="hd-inner">
+                <h2 className="heading-73">
+                  Consent Governance<br/>Platform
+                </h2>
+                <div className="div-block-21">
+                </div>
+                <div className="paragraph-14px-3">
+                  The custodian off consents
+                </div>
+              </div>
+              <div className="paragraph-16px-4">
+                A Governance platform that will streamline your organisation to collect, store and manage&nbsp;&nbsp;consents the compliant way.
+              </div>
+              <div className="div-block-20">
+                <a href="/cgp" className="link-6">
+                  Explore CGP
+                </a>
+                <img src="https://cdn.prod.website-files.com/65f7dd1d0e60ce666af29650/668e6ca84e4730a2ef3fa896_arrow_forward.png" loading="lazy" alt=""/>
+              </div>
+            </div>
+            <div data-w-id="a2ba9228-f7c1-f0c8-bf61-eb098024b5af" style={{opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d"}} className="hd-wrapper last">
+              <div className="hd-inner">
+                <h2 className="heading-74">
+                  Cookie<br/>Manager
+                </h2>
+                <div className="div-block-21">
+                </div>
+                <div className="paragraph-14px-3">
+                  The website compliance manager
+                </div>
+              </div>
+              <div className="paragraph-16px-4">
+                Platform that manages cookie consents on your website
+              </div>
+              <div className="div-block-20">
+                <a href="/category/cookies" className="link-6">
+                  Explore cookies manager
+                </a>
+                <img src="https://cdn.prod.website-files.com/65f7dd1d0e60ce666af29650/668e6ca84e4730a2ef3fa896_arrow_forward.png" loading="lazy" alt=""/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <main className="main-content">
         <h2>A comprehensive consent governance suite for all enterprises</h2>
         <div className="cards">
           <div className="card">
             <h3>Inspect AI</h3>
             <p className="tagline">The compliance co-pilot</p>
-            <p>
+            <p className="description">
               A tool designed for Privacy & Info-sec teams to perform DPDP gap
               assessments in less than a minute.
             </p>
@@ -28,7 +138,7 @@ function App() {
           <div className="card">
             <h3>Consent Governance Platform</h3>
             <p className="tagline">The custodian of consents</p>
-            <p>
+            <p className="description">
               A Governance platform that will streamline your organization to
               collect, store and manage consents the compliant way.
             </p>
@@ -37,11 +147,11 @@ function App() {
           <div className="card">
             <h3>Cookie Manager</h3>
             <p className="tagline">The website compliance manager</p>
-            <p>Platform that manages cookie consents on your website</p>
+            <p className="description">Platform that manages cookie consents on your website</p>
             <a href="#cookie">Explore Cookie Manager →</a>
           </div>
         </div>
-      </main>
+      </main> */}
     </div>
   );
 };
