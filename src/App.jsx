73ed2b51e2b168eca731e313a52cc02993fc816f
sakshi.dhenge <sakshi.dhenge@idfy.com>
2025-01-13 17:22:55 +0530

    iframes added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,55 @@ import idfyLogo from "./assets/idfy_logo.png"
 function App() {
   return (
     <div className="container">
+
+      <div id="vimeo-container"></div>
+
+      <div>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" crossOrigin="anonymous"></script>
+        <ins className="adsbygoogle" style="display:block" data-ad-client="ca-pub-1234567890123456"
+          data-ad-slot="1234567890"></ins>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({ });
+        </script>
+
+        <iframe width="480" height="270" src="https://www.dailymotion.com/embed/video/x7t8jv1"
+          allowfullscreen allow="autoplay"></iframe>
+
+        <iframe height="400" width="315" src="https://www.youtube.com/embed/O5hShUO6wxs?autoplay=1">
+        </iframe>
+      </div>
+
+      {/* <!-- Facebook Like Button --> */}
+      <div>
+        <div className="fb-like" data-href="https://cookie-manager.sanketsaboo.com" data-width="" data-layout="standard"
+          data-action="like" data-size="large" data-share="true"></div>
+
+        {/* <!-- Facebook SDK --> */}
+        <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v14.0"
+          nonce="YOUR_NONCE">
+        </script>
+      </div>
+
+      {/* <!-- Twitter Share Button --> */}
+      <div>
+        <a href="https://twitter.com/share" className="twitter-share-button" data-text="Check out this awesome site!"
+          data-url="https://cookie-manager.sanketsaboo.com" data-hashtags="example,website" data-show-count="false">
+          Tweet
+        </a>
+
+        {/* <!-- Twitter SDK --> */}
+        <script async src="https://platform.twitter.com/widgets.js"></script>
+      </div>
+      <div>
+        {/* <!-- Pinterest Save Button --> */}
+        <a data-pin-do="buttonPin" href="https://www.pinterest.com/pin/create/button/" data-pin-custom="true">
+          <img src="https://assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_20.png" />
+        </a>
+
+        {/* <!-- Pinterest SDK --> */}
+        <script async defer src="//assets.pinterest.com/js/pinit.js"></script>
+      </div>
+
       <header className="header">
         <div className="header-logo-section">
           <h1>PRIVY</h1>
@@ -50,7 +99,7 @@ function App() {
         <div className="container-12">
           <div className="home-data">
             <div className="hd-header">
-              <h2 data-w-id="877904d2-8fa1-ad57-8a62-e5aaf389adda" style={{opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d"}} className="heading-76">
+              <h2 data-w-id="877904d2-8fa1-ad57-8a62-e5aaf389adda" style={{ opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d" }} className="heading-76">
                 A comprehensive consent governance suite for all enterprises
               </h2>
               <div className="div-block-21 mt20">
@@ -58,7 +107,7 @@ function App() {
             </div>
           </div>
           <div className="hd-content">
-            <div data-w-id="a2ba9228-f7c1-f0c8-bf61-eb098024b59b" style={{opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d"}} className="hd-wrapper">
+            <div data-w-id="a2ba9228-f7c1-f0c8-bf61-eb098024b59b" style={{ opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d" }} className="hd-wrapper">
               <div className="hd-inner">
                 <h2 className="heading-75">
                   Inspect AI
@@ -70,18 +119,18 @@ function App() {
                 </div>
               </div>
               <div className="paragraph-16px-4">
-                A tool designed for Privacy &amp; Info-sec teams to perform DPDP gap assessments in less <br/>than a minute.</div>
+                A tool designed for Privacy &amp; Info-sec teams to perform DPDP gap assessments in less <br />than a minute.</div>
               <div className="div-block-20">
                 <a href="/inspect-ai" className="link-6">
                   Explore Inspect AI
                 </a>
-                <img src="https://cdn.prod.website-files.com/65f7dd1d0e60ce666af29650/668e6ca84e4730a2ef3fa896_arrow_forward.png" loading="lazy" alt=""/>
+                <img src="https://cdn.prod.website-files.com/65f7dd1d0e60ce666af29650/668e6ca84e4730a2ef3fa896_arrow_forward.png" loading="lazy" alt="" />
               </div>
             </div>
-            <div data-w-id="a2ba9228-f7c1-f0c8-bf61-eb098024b5a5" style={{opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d"}} className="hd-wrapper">
+            <div data-w-id="a2ba9228-f7c1-f0c8-bf61-eb098024b5a5" style={{ opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d" }} className="hd-wrapper">
               <div className="hd-inner">
                 <h2 className="heading-73">
-                  Consent Governance<br/>Platform
+                  Consent Governance<br />Platform
                 </h2>
                 <div className="div-block-21">
                 </div>
@@ -96,13 +145,13 @@ function App() {
                 <a href="/cgp" className="link-6">
                   Explore CGP
                 </a>
-                <img src="https://cdn.prod.website-files.com/65f7dd1d0e60ce666af29650/668e6ca84e4730a2ef3fa896_arrow_forward.png" loading="lazy" alt=""/>
+                <img src="https://cdn.prod.website-files.com/65f7dd1d0e60ce666af29650/668e6ca84e4730a2ef3fa896_arrow_forward.png" loading="lazy" alt="" />
               </div>
             </div>
-            <div data-w-id="a2ba9228-f7c1-f0c8-bf61-eb098024b5af" style={{opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d"}} className="hd-wrapper last">
+            <div data-w-id="a2ba9228-f7c1-f0c8-bf61-eb098024b5af" style={{ opacity: 1, transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", transformStyle: "preserve-3d" }} className="hd-wrapper last">
               <div className="hd-inner">
                 <h2 className="heading-74">
-                  Cookie<br/>Manager
+                  Cookie<br />Manager
                 </h2>
                 <div className="div-block-21">
                 </div>
@@ -117,13 +166,13 @@ function App() {
                 <a href="/category/cookies" className="link-6">
                   Explore cookies manager
                 </a>
-                <img src="https://cdn.prod.website-files.com/65f7dd1d0e60ce666af29650/668e6ca84e4730a2ef3fa896_arrow_forward.png" loading="lazy" alt=""/>
+                <img src="https://cdn.prod.website-files.com/65f7dd1d0e60ce666af29650/668e6ca84e4730a2ef3fa896_arrow_forward.png" loading="lazy" alt="" />
               </div>
             </div>
           </div>
         </div>
       </div>
-      
+
       {/* <main className="main-content">
         <h2>A comprehensive consent governance suite for all enterprises</h2>
         <div className="cards">
